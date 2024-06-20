0bfaca7c2eb9e3484612543984cd99f34260a6b2
Chieh <32257273+Chenqwer@users.noreply.github.com>
2024-06-20 15:40:04 +0800

    Update uart_byte_tx_tb.v

--- uart_byte_tx_tb.v
+++ uart_byte_tx_tb.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2024/02/27 09:36:43
-// Design Name: 
-// Module Name: uart_byte_tx_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module uart_byte_tx_tb();
@@ -38,7 +19,7 @@ module uart_byte_tx_tb();
         .led(led)
     );
     
-//    defparam uart_byte_tx.MCNT_DLY=125_000_0-1;//改為10ms易於仿真
+//    defparam uart_byte_tx.MCNT_DLY=125_000_0-1;
     defparam uart_byte_tx.MCNT_DLY=125_00-1;
     initial clk_p=1;
     always #4 clk_p=~clk_p;
